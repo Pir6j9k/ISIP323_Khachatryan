0e3740dcf233e8a6f38cfb9e9e3b07251f931fa3
Pir6j9k <hasulik2060@gmail.com>
2025-09-18 13:50:41 +0300

    Создание структуры проекта

--- Program.cs
+++ Program.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopInventory
+{
+    public enum Category
+    {
+
+    }
+
+    public class Product
+    {
+        
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void AddProduct()
+        {
+            
+        }
+
+        static void RemoveProduct()
+        {
+            
+        }
+
+        static void SupplyProduct()
+        {
+            
+        }
+
+        static void SellProduct()
+        {
+
+        }
+
+        static void SearchProducts()
+        {
+
+        }
+
+        static void SearchByCode(string code)
+        {
+
+        }
+
+        static void SearchByName(string name)
+        {
+        
+        }
+
+        static void SearchByCategory(Category category)
+        {
+            
+        }
+
+        static void ShowSearchResults(IEnumerable<Product> results)
+        {
+         
+        }
+
+        static void ShowAllProducts()
+        {
+           
+        }
+    }
+}
