f87265fff307fa4990d9c060a426ce81bd64af9c
Pir6j9k <hasulik2060@gmail.com>
2025-09-24 14:52:38 +0300

    возврат общего количества слов

--- Program.cs
+++ Program.cs
@@ -92,6 +92,7 @@ namespace TextAnalyzer
                             inWord = false;
                         }
                     }
+                    return wordCount;
                 }
             }
 
