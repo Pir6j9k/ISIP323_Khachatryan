e7e07c8a9f2aecd17bbff602b72c00ea98bb3d09
Pir6j9k <hasulik2060@gmail.com>
2025-09-24 15:21:00 +0300

    Поиск самого короткого и длинного слова

--- Program.cs
+++ Program.cs
@@ -98,6 +98,17 @@ namespace TextAnalyzer
                     shortest = "";
                     longest = "";
                     string[] words = SplitTextIntoWords(text);
+                    for (int i = 1;  i < words.Length; i++)
+                    {
+                        if(words[i].Length < shortest.Length)
+                        {
+                            shortest = words[i];
+                        }
+                        if(words[i].Length > longest.Length)
+                        {
+                            longest = words[i];
+                        }
+                    }
                 }
             }
 
