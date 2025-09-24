813034e5c1ba7e676f50ba1401f1b2b2f59669f7
Pir6j9k <hasulik2060@gmail.com>
2025-09-24 12:02:37 +0300

    добавление класса для хранения статистики по тексту

--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ namespace TextAnalyzer
 {
     class Program
     {
-        
+        //Класс для хранения статистики по тексту
+        class TextStats
+        {
+            public int WordCount { get; set; }
+            public string ShortestWord { get; set; }
+            public int SentenceCount { get; set; }
+            public int GlasCount { get; set; }
+            public int SoglCount { get; set; }
+            public string LongestWord { get; set; }
+            public Dictionary<char, int> LetterFrequency { get; set; }
+            public DateTime AnalyzerTime {  get; set; }
+        }
     }
 }
