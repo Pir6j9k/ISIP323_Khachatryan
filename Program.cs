1aefad563b1618664305293071045a6b97555869
Pir6j9k <hasulik2060@gmail.com>
2025-10-15 16:47:07 +0300

    Декомпозиция: добалены классы Person, Student, Teacher, Course, UniversityManager, Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace University
+{
+    public abstract class Person // Базовый класс для всех людей в университете
+    {
+
+    }
+    public class Student : Person //Класс студента - наследник Person
+    {
+
+    }
+    public class Teacher : Person //Класс преподаватель - наследник Person
+    {
+
+    }
+    public class Course //Класс курсов
+    {
+
+    }
+    public class UniversityManager //Основной класс для управления университетской системой
+    {
+
+    }
+    public class Program // Главный класс программы
+    {
+
+    }
+}
