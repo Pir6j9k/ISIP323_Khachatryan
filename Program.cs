314174e0ef750fb88e6a8c4c10f14d40674852e3
Pir6j9k <hasulik2060@gmail.com>
2025-10-21 22:47:28 +0300

    Декомпозиция: добалвение методов в классы наследники

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿public class Item
+﻿using System.Xml.Linq;
+
+public class Item
 {
         
     public Item(string name, string type, int attackBonus = 0, int defenseBonus = 0) // Конструктор класса Item для инициализации свойств
@@ -16,15 +18,40 @@
 }
 public class Weapon: Item
 {
+    // Конструктор класса Weapon, вызывает базовый конструктор с типом "Оружие"
+    public Weapon(string name, int attackBonus) 
+        : base(name, "Оружие", attackBonus, 0) 
+    {
 
+    } 
+    public override void ShowStats() // Переопределенный метод отображения статистики для оружия
+    {
+        
+    }
 }
 public class Armor: Item
 {
-
+    // Конструктор класса Armor, вызывает базовый конструктор с типом "Доспехи"
+    public Armor(string name, int defenseBonus)
+        : base(name, "Доспехи", 0, defenseBonus) { } 
+    public override void ShowStats() // Переопределенный метод отображения статистики для доспехов
+    {
+        
+    }
 }
 public class Potion: Item
 {
+    // Конструктор класса Potion, вызывает базовый конструктор с типом "Зелье"
+    public Potion(string name)
+        : base(name, "Зелье") { } 
+    public override void Use(Player player) // Переопределенный метод использования зелья
+    {
+        
+    }
+    public override void ShowStats() // Переопределенный метод отображения статистики для зелья
+    {
 
+    }
 }
 public class Player
 {
