cacd49b01fb9871a9c640d8380e5fcaa3039e024
Pir6j9k <hasulik2060@gmail.com>
2025-10-21 14:12:49 +0300

    Декомпозиция: добавление классов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Item
+{
+
+}
+public class Weapon: Item
+{
+
+}
+public class Armor: Item
+{
+
+}
+public class Potion: Item
+{
+
+}
+public class Player
+{
+
+}
+public class Enemy
+{
+
+}
+public class Goblin: Enemy
+{
+
+}
+public class Skeleton: Enemy
+{
+
+}
+public class Mage: Enemy
+{
+
+}
+public class Boss: Enemy
+{
+
+}
+public class VVG : Boss
+{
+
+}
+public class Kovalsky: Boss
+{
+
+}
+public class ArchimageCPP: Boss
+{
+
+}
+public class PestovCMM: Boss
+{
+
+}
+public class Chest
+{
+
+}
+public class Game
+{
+
+}
+class Program
+{
+
+}
